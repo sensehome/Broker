88cf3ef7aed931dd1814e9448daad8fb046d9854
Rafiul Islam <avoidcloud@gmail.com>
2021-01-07 17:48:15 +0600

    added internal service to provide accessablity of mqtt server

---
+++ src/SenseHome.Broker/Utility/BrokerCommandTopics.cs
@@ -0,0 +1,17 @@
+﻿namespace SenseHome.Broker.Utility
+{
+    public class BrokerCommandTopics
+    {
+        private static string Base { get; } = "$SYS/request/broker";
+        private static string ClientBase { get; } = $"{Base}/clients";
+
+        public string GetConnectedClients { get; } = $"{ClientBase}/connected";
+        public string GetDisconnectedClients { get; } = $"{ClientBase}/disconnected";
+        public string GetConnectedClientsCount { get; } = $"{ClientBase}/connected/count";
+        public string GetDisconnectedClientsCount { get; } = $"{ClientBase}/disconnected/count";
+        public string GetSubscriptionCount { get; } = $"{Base}/subscriptions/count";
+        public string GetClientIP { get; } = $"{ClientBase}/ip";
+        public string GetClientConnectedTime { get; } = $"{ClientBase}/connectedtime";
+        public string PatchDisconnectClient { get; } = $"{ClientBase}/disconnect/command";
+    }
+}
---
+++ src/SenseHome.Broker/Utility/BrokerEventTopics.cs
@@ -0,0 +1,25 @@
+﻿namespace SenseHome.Broker.Utility
+{
+    public class BrokerEventTopics
+    {
+        private static string Base { get; } = "$SYS/broker";
+        private static string ClientBase { get; } = $"{Base}/clients";
+
+        public string ConnectedClients { get; } = $"{ClientBase}/connected";
+        public string DisconnectedClients { get; } = $"{ClientBase}/disconnected";
+        public string NewClientConnected { get; } = $"{ClientBase}/connected/new";
+        public string NewClientDisconnected { get; } = $"{ClientBase}/disconnected/new";
+        public string ConnectedClientsCount { get; } = $"{ClientBase}/connected/count";
+        public string DisconnectedClientsCount { get; } = $"{ClientBase}/disconnected/count";
+        public string SubscriptionsCount { get; } = $"{Base}/subscriptions/count";
+        public string ClientIP(string clientId)
+        {
+            return $"{ClientBase}/{clientId}/ip";
+        }
+
+        public string ClientConnectedTime(string clientId)
+        {
+            return $"{ClientBase}/{clientId}/connectedtime";
+        }
+    }
+}
